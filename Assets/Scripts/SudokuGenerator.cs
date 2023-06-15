8ffeec23937611b8f46d0b0de2ecc49e0c0e5ec1
jakehdchoi <jakehdchoi@gmail.com>
2023-06-15 18:28:29 +0900

    8: CreateRandomGroups in generator

--- Assets/Scripts/SudokuGenerator.cs
+++ Assets/Scripts/SudokuGenerator.cs
@@ -6,6 +6,39 @@ public class SudokuGenerator
 {
     public static SudokuObject CreateSudokuObject()
     {
-        return new SudokuObject();
+        SudokuObject sudokuObject = new SudokuObject();
+        CreateRandomGroups(sudokuObject);
+        return sudokuObject;
     }
+
+    public static void CreateRandomGroups(SudokuObject sudokuObject)
+    {
+        List<int> values = new List<int>() { 0, 1, 2 };
+        int index = Random.Range(0, values.Count);
+        InsertRandomGroup(sudokuObject, 1 + values[index]);
+        values.RemoveAt(index);
+
+        index = Random.Range(0, values.Count);
+        InsertRandomGroup(sudokuObject, 4 + values[index]);
+        values.RemoveAt(index);
+
+        index = Random.Range(0, values.Count);
+        InsertRandomGroup(sudokuObject, 7 + values[index]);
+    }
+
+    public static void InsertRandomGroup(SudokuObject sudokuObject, int group)
+    {
+        sudokuObject.GetGroupIndex(group, out int startRow, out int startCol);
+        List<int> values = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        for (int row = startRow; row < startRow + 3; row++)
+        {
+            for (int col = startCol; col < startCol + 3; col++)
+            {
+                int index = Random.Range(0, values.Count);
+                sudokuObject.Values[row, col] = values[index];
+                values.RemoveAt(index);
+            }
+        }
+    }
+
 }
--- Assets/Scripts/SudokuObject.cs
+++ Assets/Scripts/SudokuObject.cs
@@ -5,4 +5,51 @@ using UnityEngine;
 public class SudokuObject
 {
     public int[,] Values = new int[9, 9];
+
+    public void GetGroupIndex(int group, out int startRow, out int startCol)
+    {
+        startRow = 0;
+        startCol = 0;
+        switch (group)
+        {
+            case 1:
+                startRow = 0;
+                startCol = 0;
+                break;
+            case 2:
+                startRow = 0;
+                startCol = 3;
+                break;
+            case 3:
+                startRow = 0;
+                startCol = 6;
+                break;
+            case 4:
+                startRow = 3;
+                startCol = 0;
+                break;
+            case 5:
+                startRow = 3;
+                startCol = 3;
+                break;
+            case 6:
+                startRow = 3;
+                startCol = 6;
+                break;
+            case 7:
+                startRow = 6;
+                startCol = 0;
+                break;
+            case 8:
+                startRow = 6;
+                startCol = 3;
+                break;
+            case 9:
+                startRow = 6;
+                startCol = 6;
+                break;
+
+        }
+
+    }
 }
